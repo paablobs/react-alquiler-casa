4855ec8ac84ccaca004db7055a2851b6744a4a61
Pablo <pablo.bessone@outlook.com>
2020-10-11 20:07:34 -0300

    WIP Footer + Header
    * FIX margin in both header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import "../../css/footer.css";
 
 class Footer extends Component {
   render() {
-    return <div>FOOTER!</div>;
+    return <div className="footer">FOOTER!</div>;
   }
 }
 
 export default Footer;
+
+// TODO FIX MARGEN QUE SALE DE LA NADA
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
+import { Grid } from "@material-ui/core";
 import React, { Component } from "react";
+import Logo from "./Logo";
+import Menu from "./Menu";
+import "../../css/header.css";
 
 class Header extends Component {
   render() {
-    return <div>HEADER!</div>;
+    return (
+      <div className="header">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item xs>
+            <Logo />
+          </Grid>
+          <Grid item xs={6} />
+          <Grid item xs>
+            <Menu />
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
 export default Header;
+
+// TODO FIX MARGEN QUE SALE DE LA NADA
